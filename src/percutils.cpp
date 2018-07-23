e60ee5dc2589789d79d21660026d6ad9c635df51
theperkinrex <theperkinrex@gmail.com>
2018-07-23 14:54:49 +0200

    Initial Commit

---
+++ src/percutils.cpp
@@ -0,0 +1,19 @@
+//
+//  percutils.cpp
+//  percutils
+//
+//  Created by Juan on 23/7/18.
+//  Copyright © 2018 ThePerkinrex. All rights reserved.
+//
+
+#include "percutils.hpp"
+#include <iostream>
+
+void print(char * str){
+    std::cout << str;
+}
+
+void println(char * str){
+    print(str);
+    std::cout << "";
+}
---
+++ src/percutils.hpp
@@ -0,0 +1,18 @@
+//
+//  percutils.hpp
+//  percutils
+//
+//  Created by Juan on 23/7/18.
+//  Copyright © 2018 ThePerkinrex. All rights reserved.
+//
+
+#ifndef percutils_hpp
+#define percutils_hpp
+
+#include <stdio.h>
+
+#endif /* percutils_hpp */
+
+void print(char * str);
+
+void println(char * str);
